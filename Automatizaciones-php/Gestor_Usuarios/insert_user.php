32b766f6b1739b0813111562a5ac6acc993b63c2
Santiago2276 <148465074+Santiago2276@users.noreply.github.com>
2024-03-15 10:55:02 -0500

    Add files via upload

--- Automatizaciones-php/Gestor_Usuarios/insert_user.php
+++ Automatizaciones-php/Gestor_Usuarios/insert_user.php
@@ -13,6 +13,19 @@ $telefono = $_POST['telefono'];
 
 $checkRol = mysqli_query($con, "SELECT * FROM tblroles WHERE Nombre = '$rol'");
 if (mysqli_num_rows($checkRol) > 0) {
+
+    // Verificar si el cliente existe en la tabla de usuarios y tiene el rol "Cliente"
+// Verificar si la cédula ya está registrada
+$sql_verify_cedula = "SELECT Cedula FROM tblusuarios WHERE Cedula = ?";
+$stmt_verify_cedula = mysqli_prepare($con, $sql_verify_cedula);
+mysqli_stmt_bind_param($stmt_verify_cedula, 's', $cedula);
+mysqli_stmt_execute($stmt_verify_cedula);
+mysqli_stmt_store_result($stmt_verify_cedula);
+
+if (mysqli_stmt_num_rows($stmt_verify_cedula) > 0) {
+    echo '<script>alert("La cédula ya está registrada."); window.location.href = "usuario.php";</script>';
+    exit();
+}
     
     $sql = "INSERT INTO tblusuarios (cedula, correo, nombre, Password, rol, telefono) VALUES ('$cedula', '$correo', '$nombre', '$Password', '$rol', '$telefono')";
     $query = mysqli_query($con, $sql);
